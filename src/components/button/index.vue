24741707fd6b2df6d18857aec24e9392cb886f1f
Qiurong <3329279429@qq.com>
2022-07-18 20:12:29 +0800

    '按钮大小'

--- src/components/button/index.vue
+++ src/components/button/index.vue
@@ -1,5 +1,5 @@
 <template>
-  <button class="qiu-button-primary" disabled is-round>
+  <button class="qiu-button" :class="[isSize]">
     <slot></slot>
   </button>
 </template>
@@ -7,6 +7,33 @@
 <script>
 export default {
   name: "index",
+  props: {
+    type: {
+      type: String,
+      default: "",
+    },
+    size: {
+      type: String,
+      default: "",
+    },
+    round: Boolean,
+    border: Boolean,
+    disabled: Boolean,
+  },
+  computed: {
+    theme() {
+      return this.type ? `qiu-button-${this.type}` : "";
+    },
+    isRound() {
+      return this.round ? "is-round" : "";
+    },
+    isBorder() {
+      return this.border ? "is-border" : "";
+    },
+    isSize() {
+      return this.size ? `qiu-button-${this.size}` : "";
+    },
+  },
 };
 </script>
 
@@ -22,29 +49,69 @@ export default {
   font-size: 14px;
   color: #606266;
   cursor: pointer;
+
+  + .yang-button {
+    margin-left: 14px;
+  }
+}
+.qiu-button-medium {
+  height: 38px;
+}
+.qiu-button-small {
+  padding: 0 15px;
+  height: 32px;
+  font-size: 12px;
+}
+.qiu-button-mini {
+  padding: 0 15px;
+  height: 28px;
+  font-size: 12px;
+}
+.qiu-button[disabled] {
+  cursor: not-allowed;
+  opacity: 0.5;
 }
 .qiu-button-primary {
   background-color: #409eff;
   border-color: #409eff;
   color: #fff;
+  &.is-border {
+    background-color: transparent;
+    color: #409eff;
+  }
 }
 .qiu-button-success {
   background-color: #00d100;
   border-color: #00d100;
   color: #fff;
+  &.is-border {
+    background-color: transparent;
+    color: #00d100;
+  }
 }
 .qiu-button-danger {
   background-color: #e6a23c;
   border-color: #e6a23c;
   color: #fff;
+  &.is-border {
+    background-color: transparent;
+    color: #e6a23c;
+  }
 }
 .qiu-button-warning {
   background-color: #f56c6c;
   border-color: #f56c6c;
   color: #fff;
+  &.is-border {
+    background-color: transparent;
+    color: #f56c6c;
+  }
 }
-.qiu-button[disabled] {
-  cursor: not-allowed;
-  opacity: 0.5;
+
+/**
+ * 圆角
+ */
+.is-round {
+  border-radius: 100px;
 }
 </style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,7 +2,7 @@
   <div class="home">
     <qiuButton>默认按钮</qiuButton>
     <qiuButton type="success">成功按钮</qiuButton>
-    <qiuButton type="danger">危险按钮</qiuButton>
+    <qiuButton type="danger" size="mini">危险按钮</qiuButton>
     <qiuButton type="warning">警告按钮</qiuButton>
   </div>
 </template>
